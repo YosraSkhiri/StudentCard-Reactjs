9f719043b2df268eab418a535b387d3a04823bd0
Yosra <yos.skhiri@gmail.com>
2020-09-02 16:10:29 +0100

    added the principal components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
 
+import Nav from './components/Nav';
+import FormInfo from './components/FormInfo';
+import StudentCard from './components/StudentCard';
+
 function App() {
   return (
     <div>
+      <Nav />
+      <main>
+        <StudentCard />
+        <FormInfo />
+      </main>
       
     </div>
   );
---
+++ src/components/StudentCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function StudentDard() {
+    return(
+        <div>
+
+        </div>
+    );
+}
+
+export default StudentDard;
