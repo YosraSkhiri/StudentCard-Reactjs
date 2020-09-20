930c1d74402fe4c98a8e6fd1c12556dbf460e655
Yosra <yos.skhiri@gmail.com>
2020-09-21 00:49:01 +0100

    made the card shadow change with the card color

--- src/components/StudentCard.js
+++ src/components/StudentCard.js
@@ -20,12 +20,36 @@ function StudentDard() {
     } = useContext(InputContext); 
 
 
-    const [bgColor, setBgColor] = useState({
-        backgroundImage: 'linear-gradient(45deg, #00CEEF 0%, #1EF0FF 50%, #017CF3 100%)'
+    const [cardStyle, setCardStyle] = useState({
+        backgroundImage: 'linear-gradient(45deg, #00CEEF 0%, #1EF0FF 50%, #017CF3 100%)',
+        boxShadow: `0 1px 2px rgba(1, 124, 243, 0.2),
+                    0 2px 4px rgba(1, 124, 243, 0.2),
+                    0 4px 8px rgba(1, 124, 243, 0.2), 
+                    0 8px 16px rgba(1, 124, 243, 0.2),
+                    0 16px 32px rgba(1, 124, 243, 0.2)`
     })
 
+    const hexToRGBa_CardShadow = (hex) => {
+        let r = parseInt(hex.slice(1, 3), 16),
+            g = parseInt(hex.slice(3, 5), 16),
+            b = parseInt(hex.slice(5, 7), 16);
+        
+        let cardShadow = '0 1px 2px rgba(' + r + ', ' + g + ', ' + b + ', 0.2),' +
+                         '0 2px 4px rgba(' + r + ', ' + g + ', ' + b + ', 0.2),'+
+                         '0 4px 8px rgba(' + r + ', ' + g + ', ' + b + ', 0.2),'+
+                         '0 8px 16px rgba(' + r + ', ' + g + ', ' + b + ', 0.2),'+
+                         '0 16px 32px rgba(' + r + ', ' + g + ', ' + b + ', 0.2)';
+
+        return cardShadow;
+    }
+
     const changeBgCard = (NewBg) => {
-        setBgColor(NewBg);
+        let hexColor = NewBg.substr(-13, 7);
+        let cardShadow = hexToRGBa_CardShadow(hexColor);
+        setCardStyle({
+            backgroundImage: NewBg,
+            boxShadow: cardShadow
+        });
     }
 
     const handleChange = (e) => {
@@ -35,7 +59,7 @@ function StudentDard() {
 
     return(
         <div>
-        <div className={styles.studentCard} style={{backgroundImage: bgColor}}>
+        <div className={styles.studentCard} style={cardStyle}>
             <div className={styles.studentCard__generalInfo}>
                 <div className={styles.studentCard__uni}>{ uniName }</div>
                 <div className={styles.studentCard__year}>
