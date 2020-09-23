e9c9ca1f67e46492dfaf37287d8846e55783109e
Yosra <yos.skhiri@gmail.com>
2020-09-23 22:16:26 +0100

    Updates

---
+++ precache-manifest.d56e8e8120684dde4c02408138ec423b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f17844261ed85379ef257893284e976e",
+    "url": "/StudentCard-Reactjs/index.html"
+  },
+  {
+    "revision": "5bc4c992d3a60e7afd34",
+    "url": "/StudentCard-Reactjs/static/css/main.2da5c506.chunk.css"
+  },
+  {
+    "revision": "867d47347e790e377243",
+    "url": "/StudentCard-Reactjs/static/js/2.36e6fefe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/StudentCard-Reactjs/static/js/2.36e6fefe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bc4c992d3a60e7afd34",
+    "url": "/StudentCard-Reactjs/static/js/main.b03c260e.chunk.js"
+  },
+  {
+    "revision": "7f3821e08a65fac133ca",
+    "url": "/StudentCard-Reactjs/static/js/runtime-main.e93319bc.js"
+  },
+  {
+    "revision": "2a0e46d08e0dad9f8ba6acfb5b7520d0",
+    "url": "/StudentCard-Reactjs/static/media/upload_icon.2a0e46d0.svg"
+  }
+]);
