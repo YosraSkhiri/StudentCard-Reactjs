cf2a3ed0a6eb04af0f0c63d9578153b7075091f6
Yosra <yos.skhiri@gmail.com>
2020-09-24 01:08:05 +0100

    demo gif in the readme file

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # StudentCard-Reactjs
 ** this app is not responsive and was made for learning purposes. **
 ## App Demo
-![App Demo](https://github.com/YosraSkhiri/StudentCard-Reactjs/tree/master/src/img/React-App.gif)
+![App Demo](https://github.com/YosraSkhiri/StudentCard-Reactjs/blob/master/src/img/React-App.gif)
--- src/components/StudentCard.js
+++ src/components/StudentCard.js
@@ -21,6 +21,15 @@ function StudentDard({ showColorOption }) {
     } = useContext(InputContext); 
 
 
+    const [cardStyle, setCardStyle] = useState({
+        backgroundImage: 'linear-gradient(45deg, #00CEEF 0%, #1EF0FF 50%, #017CF3 100%)',
+        boxShadow: `0 1px 2px rgba(1, 124, 243, 0.2),
+                    0 2px 4px rgba(1, 124, 243, 0.2),
+                    0 4px 8px rgba(1, 124, 243, 0.2), 
+                    0 8px 16px rgba(1, 124, 243, 0.2),
+                    0 16px 32px rgba(1, 124, 243, 0.2)`
+    });
+
     const hexToRGBa_CardShadow = (hex) => {
         let r = parseInt(hex.slice(1, 3), 16),
             g = parseInt(hex.slice(3, 5), 16),
@@ -35,6 +44,15 @@ function StudentDard({ showColorOption }) {
         return cardShadow;
     }
 
+    const changeBgCard = (NewBg) => {
+        let hexColor = NewBg.substr(-13, 7);
+        let cardShadow = hexToRGBa_CardShadow(hexColor);
+        setCardStyle({
+            backgroundImage: NewBg,
+            boxShadow: cardShadow
+        });
+    }
+
     const handleChange = (e) => {
         if(e.target.files[0]) {
             addStudentImg(URL.createObjectURL(e.target.files[0])); 
@@ -43,7 +61,7 @@ function StudentDard({ showColorOption }) {
 
     return(
         <div>
-        <div className={styles.studentCard} style={ {backgroundImage: studentCardBg} }>
+        <div className={styles.studentCard} style={cardStyle}>
             <div className={styles.studentCard__generalInfo}>
                 <div className={styles.studentCard__uni}>{ uniName }</div>
                 <div className={styles.studentCard__year}>
@@ -86,7 +104,7 @@ function StudentDard({ showColorOption }) {
                 </div>
             </div>
         </div>
-        { showColorOption ? <BgCardColor /> : null}
+        { showColorOption ? <BgCardColor parentCallback={changeBgCard}/> : null}
         </div>
     );
 }
